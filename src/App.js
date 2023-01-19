19f6861b1e695c0f2bba75f1c5d5a96e7b112829
Jasurbek Shomaqsudov <107366287+Jasurbek2208@users.noreply.github.com>
2023-01-19 18:04:21 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 import TelegramBot from 'telegram-bot-api';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [messageSent, setMessageSent] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
     const bot = new TelegramBot('5117041696:AAG2IRH4POh4BWTZcF_trTkJat2ERQ6ckME');
     try {
       await bot.sendMessage({
         chat_id: '5117041696',
         text: message
       });
-      alert('message sent successfully!');
+      setMessageSent(true);
     } catch (error) {
       console.log(error);
-      alert('Error sending message');
+      setMessageSent(false);
     }
+    setLoading(false);
   }
 
   return (
@@ -28,8 +32,12 @@ function App() {
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button type="submit">Send</button>
+        <button type="submit" disabled={loading}>
+          {loading ? 'Sending...' : 'Send'}
+        </button>
       </form>
+      {messageSent === true && <p>Message sent successfully!</p>}
+      {messageSent === false && <p>Error sending message</p>}
     </>
   );
 }
