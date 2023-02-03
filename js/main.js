f25bbf6a1e55dc2e75696c1899de698e46c96af8
Jasurbek Shomaqsudov <107366287+Jasurbek2208@users.noreply.github.com>
2023-02-03 12:34:18 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const CHAT_ID = '-1001680103791';
 const TOKEN = '5833819728:AAH1dRS8nucWa5_Mh_CmoKUJOIx5uTsYg6I';
-const API_URL = `https://api.telegram.org/bot${TOKEN}/sendDocument`;
-// const API_URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+const API_URL = `https://api.telegram.org/bot${TOKEN}/`;
 
 // button
 const btn = document.getElementById('form-btn');
@@ -17,34 +16,40 @@ success.className = "success-text";
 // Form submit
 document.getElementById('form').addEventListener('submit', async function (e) {
     e.preventDefault();
-    const img = this.file.files[0];
-    console.log(img);
-    let message = `<b>${this.text.value}</b>`;
     btn.setAttribute("disabled", "true");
-    
+
+    const img = this.file.files[0];
+    let message = `<b>${this.text.value}</b>`;
+    let CURRENT_API_URL = API_URL + (img === undefined ? "sendMessage" : "sendDocument");
+
     try {
-        await axios.post(API_URL, {
+        await axios.post(CURRENT_API_URL, {
             chat_id: CHAT_ID,
             parse_mode: "html",
             document: img,
+            text: message,
             caption: message,
         }, {
             headers: {
                 "Content-Type": 'multipart/form-data',
             }
         });
+
         success.textContent = "Message sent successfully!"
+
         error.classList.remove("On");
         success.classList.add("On");
+        
         this.btn.appendChild(success);
     } catch (err) {
         error.textContent = err.response.data.description + "!";
+
         success.classList.remove("On");
         error.classList.add("On");
+
         this.text.classList.add("On");
         this.btn.appendChild(error);
     } finally {
-        console.log("final logics");
         form.reset();
         btn.removeAttribute("disabled");
     }
